62626d95092d4049824f810a07ff7f06acbe688b
Jonas Örnfelt <jonasornfelt@outlook.com>
2025-07-30 00:14:09 +0200

    update tbc macros

---
+++ tbc/WTF/Account/MANGOS/MaNGOS/Vim/SavedVariables/!Questie.lua
@@ -0,0 +1,44 @@
+
+QuestieSeenQuests = {
+}
+QuestieCachedQuests = {
+}
+QuestieConfig = {
+	["trackerMinimize"] = false,
+	["alwaysShowObjectives"] = true,
+	["maxShowLevel"] = 7,
+	["showToolTips"] = true,
+	["clusterQuests"] = true,
+	["arrowTime"] = true,
+	["showMapNotes"] = true,
+	["maxLevelFilter"] = true,
+	["hideObjectives"] = false,
+	["getVersion"] = "3.7.1",
+	["arrowEnabled"] = true,
+	["corpseArrow"] = true,
+	["trackerAlpha"] = 0.6,
+	["minShowLevel"] = 4,
+	["useQuestLinks"] = true,
+	["trackerBackground"] = false,
+	["trackerScale"] = 1,
+	["hideMinimapIcons"] = false,
+	["showProfessionQuests"] = false,
+	["minimapZoom"] = false,
+	["minLevelFilter"] = true,
+	["trackerEnabled"] = true,
+	["resizeWorldmap"] = false,
+	["trackerList"] = false,
+	["showTrackerHeader"] = false,
+	["minimapButton"] = true,
+	["boldColors"] = false,
+}
+QuestieTrackerVariables = {
+	["position"] = {
+		["relativeTo"] = "UIParent",
+		["point"] = "CENTER",
+		["relativePoint"] = "CENTER",
+		["yOfs"] = 0,
+		["xOfs"] = 0,
+	},
+}
+QuestieMinimapPosition = -90
--- tbc/WTF/Account/MANGOS/SavedVariables/StormforgePvP.lua
+++ tbc/WTF/Account/MANGOS/SavedVariables/StormforgePvP.lua
@@ -1,6 +1,7 @@
 
 StormforgePvPSaved = {
 	["minimap"] = {
+		["minimapPos"] = 189.1579164614468,
 		["hide"] = false,
 	},
 }
